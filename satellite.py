68ba337a6bd3aa6cc71a8dcc1423103e57a22c82
Petri Avikainen <petri.avikainen@gmail.com>
2016-08-22 19:13:08 +0300

    calculate neighbours

--- satellite.py
+++ satellite.py
@@ -1,5 +1,7 @@
 from math import sin, cos, sqrt
 
+EARTH_RADIUS = 6371.0
+
 class Satellite:
     def __init__(self, data):
         self.name = data["name"]
@@ -7,19 +9,27 @@ class Satellite:
 
         self.position = data["position"]
         self.coordinates = self.cartesian_coordinates()
-        print self.position
-        print self.coordinates
 
     def build_neighbours(self, other_satellites):
         for satellite in other_satellites:
             if self.sees_other(satellite):
                 self.neighbours.append((self.distance_to(satellite), satellite))
 
-    def distance_to(self, satellite):
-        return sqrt(0)
+    def distance_to(self, other):
+        x1, y1, z1 = self.coordinates
+        x2, y2, z2 = other.coordinates
+        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 -z1, 2))
 
-    def sees_other(self, satellite):
-        return True
+
+    def sees_other(self, other):
+        #http://paulbourke.net/geometry/circlesphere/index.html#linesphere
+        x1, y1, z1 = self.coordinates
+        x2, y2, z2 = other.coordinates
+        a = pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 -z1, 2)
+        b = 2 * ((x2 - x1) * x1 + (y2 - y1) * y1 + (z2 - z1) * z1)
+        c = pow(x1, 2) + pow(y1, 2) + pow(z1, 2) - pow(EARTH_RADIUS, 2)
+        discriminant = pow(b, 2) - 4 * a * c
+        return discriminant < 0
 
     def cartesian_coordinates(self):
         lat, lon, height = self.position
